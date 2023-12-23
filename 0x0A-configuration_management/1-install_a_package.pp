b5c5a67fdc42f9c649dd571ed8b402db12cc643f
mmgmolotov <mmgmolotov@gmail.com>
2023-12-23 03:39:38 +0100

    fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
-##!/usr/bin/pup
-# Using Puppt to install flask from pip3
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+#Install flask version 2.1.0
+
+
+exec {'pip3 install flask':
+require => Exec['python-installed'],
+command => '/usr/bin/pip3 install flask==2.1.0'
+}
+
+exec {'python-installed':
+command => '/usr/bin/which python3'
 }
