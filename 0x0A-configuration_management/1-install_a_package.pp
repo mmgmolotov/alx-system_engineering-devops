31ee7df3885b1b1c9f784f4598bb67acc1a36d90
mmgmolotov <mmgmolotov@gmail.com>
2023-12-23 03:32:51 +0100

    fixx

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
 # 1-install_a_package.pp
 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip',
-  command  => '/usr/bin/pip3', # Path to the pip3 binary
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => '/usr/bin',
+  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
 }
