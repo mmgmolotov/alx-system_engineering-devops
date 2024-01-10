70c3702e744bc04f2926d5a3499d37e10e9a1d48
mmgmolotov <mmgmolotov@gmail.com>
2024-01-10 04:15:58 +0100

    fix

--- 0x0F-load_balancer/100-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/100-puppet_custom_http_response_header.pp
@@ -1,30 +1,56 @@
+#!/usr/bin/env bash
+## Installs Nginx with puppet with the following configurations:
+#+    Listens on port 80.
+#+    Returns a page containing "Hello World!" when queried
+#+     at the root with a curl GET request.
+#+    Configures /redirect_me as a "301 Moved Permanently".
+#+    Includes a custom 404 page containing "Ceci n'est pas une page".
+#+    Contains a custom HTTP header named X-Served-By.
+#+    The value of the HTTP header is the hostname of the running server.
 
-# Custom HTTP header in a nginx server
-
-# update ubuntu server
-exec { 'update server':
-  command  => 'apt-get update',
-  user     => 'root',
-  provider => 'shell',
-}
-->
-# install nginx web server on server
 package { 'nginx':
-  ensure   => present,
-  provider => 'apt'
+  ensure => installed,
 }
-->
-# custom Nginx response header (X-Served-By: hostname)
-file_line { 'add HTTP header':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => 'add_header X-Served-By $hostname;'
+
+file { '/var/www/html':
+  ensure => directory,
 }
-->
-# start service
+
+file { '/var/www/html/index.html':
+  ensure  => present,
+  content => 'Hello World!',
+}
+
+file { '/var/www/html/404.html':
+  ensure  => present,
+  content => "Ceci n'est pas une page",
+}
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => present,
+  content => '
+    server {
+      listen 80 default_server;
+      listen [::]:80 default_server;
+      add_header X-Served-By $hostname;
+      root /var/www/html;
+      index index.html index.htm;
+
+      location /redirect_me {
+        return 301 https//youtube.com/;
+      }
+
+      error_page 404 /404.html;
+      location /404 {
+        root /var/www/html;
+        internal;
+      }
+    }
+  ',
+}
+
 service { 'nginx':
-  ensure  => 'running',
+  ensure  => running,
   enable  => true,
-  require => Package['nginx']
+  require => File['/etc/nginx/sites-available/default'],
 }
