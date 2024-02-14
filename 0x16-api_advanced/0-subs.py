98e22d81af64ec5430ddfe31341324ee8cae59c0
mmgmolotov <mmgmolotov@gmail.com>
2024-02-14 20:30:06 +0100

    a

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,25 +1,15 @@
 #!/usr/bin/python3
-"""
-The function retrieves the total number of subscribers
-for a specified subreddit by querying the Reddit API.
-If an invalid subreddit name is provided,
-the function returns 0.
-"""
-
+"""comment """
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Function that queries the Reddit API
-    - If not a valid subreddit, return 0.
-    """
-    req = requests.get(
-        "https://www.reddit.com/r/{}/about.json".format(subreddit),
-        headers={"User-Agent": "Custom"},
-    )
+    """ get how many subscribers """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    response = requests.get(url, headers={})
 
-    if req.status_code == 200:
-        return req.json().get("data").get("subscribers")
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
     else:
         return 0
