240016367ac9b11020eb0f0633ba274e6d55aa0f
mmgmolotov <mmgmolotov@gmail.com>
2024-02-24 15:54:18 +0100

    Refactor number_of_subscribers function to use f-strings and update API endpoint

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,20 +1,15 @@
 #!/usr/bin/python3
-"""
-Queries the Reddit API and returns the number of total subscribers for a given
-subreddit.
-"""
+""" Get how many subscribers """
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """
-    Queries the Reddit API and returns the number of total subscribers for a
-    given subreddit.
-    """
-    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'Python/1.0(Holberton School 0x16 task 0)'}
+    """ get how many subscribers """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers)
-    if (not response.ok):
+
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else:
         return 0
-    subscriber_count = response.json().get('data').get('subscribers')
-    return subscriber_count
