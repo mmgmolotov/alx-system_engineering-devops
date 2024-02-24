c0027d5688256e86b7d51cd82f71cdf4ab1e37b0
mmgmolotov <mmgmolotov@gmail.com>
2024-02-24 15:52:00 +0100

    Refactor number_of_subscribers function to use updated Reddit API endpoint

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
-"""Module for task 0"""
+"""
+Queries the Reddit API and returns the number of total subscribers for a given
+subreddit.
+"""
+import requests
 
 
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API and returns the number of subscribers
-    to the subreddit"""
-    import requests
-
-    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
-                            .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
-    if sub_info.status_code >= 300:
+    """
+    Queries the Reddit API and returns the number of total subscribers for a
+    given subreddit.
+    """
+    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'Python/1.0(Holberton School 0x16 task 0)'}
+    response = requests.get(url, headers=headers)
+    if (not response.ok):
         return 0
-
-    return sub_info.json().get("data").get("subscribers")
+    subscriber_count = response.json().get('data').get('subscribers')
+    return subscriber_count
