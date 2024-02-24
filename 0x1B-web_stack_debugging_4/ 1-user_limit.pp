a48a39de02ec9d2470f564f2b3e5ffafcada4039
mmgmolotov <mmgmolotov@gmail.com>
2024-02-24 11:22:07 +0100

    Fix user limit and increase traffic in Nginx configuration

--- 0x1B-web_stack_debugging_4/ 1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/ 1-user_limit.pp
@@ -1,13 +1,12 @@
-# Increases the amount of traffic an Nginx server can handle.
+# Traffic
+exec {'increase traffic':
+  onlyif  => 'test -e /etc/default/nginx',
+  command => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
 
-# Increase the ULIMIT of the default file
-exec { 'fix--for-nginx':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin/'
-} ->
-
-# Restart Nginx
-exec { 'nginx-restart':
+# nginx need to be restared 
+exec { 'nginx restart':
   command => 'nginx restart',
   path    => '/etc/init.d/'
 }
