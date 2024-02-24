5a060958e986b268cb57aa226dad8b24bb893984
mmgmolotov <mmgmolotov@gmail.com>
2024-02-24 15:56:45 +0100

    Fix API request headers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,11 +2,11 @@
 """ Get how many subscribers """
 import requests
 
+
 def number_of_subscribers(subreddit):
     """ get how many subscribers """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers={})
 
     if response.status_code == 200:
         data = response.json()
